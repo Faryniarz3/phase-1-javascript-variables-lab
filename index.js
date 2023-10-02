3530165c4ddda14a3f2b387ed30cc6d316019cf4
Andrew Faryniarz <afaryniarz3@gmail.com>
2023-10-02 11:46:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+//let companyName = 'Scuber';
+const companyName ='Scuber';
+
+//describe('mostProfitableNeighborhood', function () {
+  //  it('is declared as equal to Chelsea', function () {
+ //     expect(mostProfitableNeighborhood).to.equal('Chelsea');
+ //   });
+
+ let mostProfitableNeighborhood = 'Chelsea'
+
+ let companyCeo = 'Susan Smith'
